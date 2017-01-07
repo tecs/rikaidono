5ad3f7e6b61728dd0f48718d87ab3ea9d5453cc7
Alexander Ivanov <ttecss@gmail.com>
2017-01-07 03:25:26 +0100

    Made the installer work on all platforms

--- setup.js
+++ setup.js
@@ -1,13 +1,35 @@
 'use strict';
 
-// TODO: Find this automatically
-const discordPath = '/usr/share/discord-canary/resources';
-const unpackPath = __dirname + '/unpack';
-const targetPath = discordPath + '/rikai';
-const extensionPath = targetPath + '/rikaikun';
-const asarPath = discordPath + '/app.asar';
-
 const fs = require('fs-extra');
+const path = require('path');
+
+let discordPath = '';
+
+switch (process.platform) {
+    case 'darwin':
+        discordPath = '/Applications/Discord.app/Contents/Resources';
+        break;
+
+    case 'win32':
+        discordPath = 'C:\\Users\\' + process.env.USERPROFILE + '\\AppData\\Local\\Discord\\';
+        discordPath += fs.readdirSync(discordPath).filter(file => fs.lstatSync(discordPath + file).isDirectory())[0];
+        discordPath += '\\lib\\net45\\resources';
+        break;
+
+    case 'linux':
+        discordPath = '/usr/share/discord-canary/resources';
+        break;
+
+    default:
+        console.error('But, why?');
+        process.exit();
+}
+
+const unpackPath = path.join(__dirname, 'unpack');
+const targetPath = path.join(discordPath, 'rikai');
+const extensionPath = path.join(targetPath, 'rikaikun');
+const asarPath = path.join(discordPath, 'app.asar');
+
 const asar = require('asar');
 const ghdownload = require('github-download');
 
@@ -16,7 +38,7 @@ const install = () => {
     asar.extractAll(asarPath, unpackPath);
 
     console.log('Patching Discord source...');
-    const indexFile = fs.readFileSync(unpackPath + '/index.js')
+    const indexFile = fs.readFileSync(path.join(unpackPath, 'index.js'))
         .toString()
         .replace(
             /(mainWindow\.loadURL)/,
@@ -26,18 +48,18 @@ const install = () => {
             /(webPreferences: {)/,
             "$1\n        webSecurity: false,"
         );
-    fs.writeFileSync(unpackPath + '/index.js', indexFile);
+    fs.writeFileSync(path.join(unpackPath, 'index.js'), indexFile);
 
     console.log('Creating a new application archive...');
-    asar.createPackage(unpackPath, __dirname + '/app.asar', e => {
+    asar.createPackage(unpackPath, path.join(__dirname, 'app.asar'), e => {
         if (e) {
             return console.error(e);
         }
 
         console.log('Creating a backup and deploying patched application...');
         fs.renameSync(asarPath, asarPath + '.bak');
-        fs.renameSync(__dirname + '/app.asar', asarPath);
-        fs.copySync(__dirname + '/src', targetPath);
+        fs.renameSync(path.join(__dirname, 'app.asar'), asarPath);
+        fs.copySync(path.join(__dirname, 'src'), targetPath);
 
         console.log('Cleaning up temporary files...');
         fs.removeSync(unpackPath);
